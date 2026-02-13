8402ad784995d23c4780141981efa1ed58adecc5
YoungYun1181 <youngyun1181@gmail.com>
2026-02-13 10:22:01 +0900

    comment

--- app3/src/pages/boardView.jsx
+++ app3/src/pages/boardView.jsx
@@ -1,11 +1,75 @@
 import { useEffect, useState } from "react"
 import { api } from '@utils/network.js'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const BoardView = () => {
 	const styles = { "resize": "none", "overFlow": "hidden" }
-	
+	const nav = useNavigate()
+	const [title, setTitle] = useState("")
+	const [content, setContent] = useState("")
+	const param = useParams().no
+	const [boardEmail, setBoardEmail] = useState('')
+	const [userEmail, setUserEmail] = useState('')
+	const [commentList, setCommentList] = useState([])
+	const [reset, setReset] = useState(false)
+	const [commentCont, setCommentCont] = useState('')
+	const [clickCom, setClickCom] = useState('')
+	const [editCom, setEditCom] = useState('')
 
+	function onload() {
+		document.querySelectorAll(".auto-resize").forEach(textarea => {
+			textarea.addEventListener("input", function () {
+				this.style.height = "auto";
+				this.style.height = this.scrollHeight + "px";
+			});
+		});
+	}
+	useEffect(() => {
+		api.post(`/boardview/${param}`).then(res => {
+			setTitle(res.data.boardData["title"])
+			setContent(res.data.boardData["content"])
+			setBoardEmail(res.data.boardData["userEmail"])
+		})
+		api.post('/me').then(res => {
+			if (res.data.status) {
+				setUserEmail(res.data.user["email"])
+			}
+		})
+		api.post(`/comment/${param}`).then(res => {
+			setCommentList(res.data.commentData)
+		})
+		setClickCom('')
+	}, [reset])
 
+	const mode = (userEmail === boardEmail)
+
+	const boardDelClick = () => {
+		api.post(`/boarddel/${param}`)
+		alert("삭제되었습니다")
+		nav('/')
+	}
+
+	const commentDelClick = (commentNo) => {
+		api.post(`/commentdel/${param}`, { commentNo })
+		alert("삭제되었습니다")
+		setReset(!reset)
+	}
+
+	const addComment = () => {
+		api.post(`/commentadd/${param}`, { userEmail, commentCont })
+		alert("댓글이 등록되었습니다")
+		setReset(!reset)
+	}
+
+	const commentEdit = (commentNo) => {
+		console.log(editCom)
+		if (editCom) {
+			api.post(`/commentedit`, {editCom, commentNo})
+			alert("댓글이 수정되었습니다")
+			setEditCom('')
+		}
+		setReset(!reset)
+	}
 
 	return (
 		<div className="container mt-3">
@@ -13,90 +77,110 @@ const BoardView = () => {
 			<form>
 				<div className="mb-3 mt-3">
 					<label htmlFor="title" className="form-label">제목</label>
-					<input type="text" className="form-control" id="title" name="title" readOnly="readonly" />
-				</div>
-				<div className="mb-3 mt-3">
-					<label htmlFor="name" className="form-label">작성자</label>
-					<input type="text" className="form-control" id="name" name="name" readOnly="readonly" />
-				</div>
-				<div className="mb-3 mt-3">
-					<label htmlFor="regDate" className="form-label">작성날짜</label>
-					<input type="text" className="form-control" id="regDate" name="regDate" readOnly="readonly" />
+					<input type="text" className="form-control" id="title" name="title" readOnly="readOnly" value={title} />
 				</div>
 				<div className="mb-3 mt-3">
 					<label htmlFor="content" className="form-label">내용</label>
-					<textarea type="text" className="form-control h-50" style={styles} rows="10" id="content" name="content" readOnly="readonly"></textarea>
+					<textarea type="text" className="form-control h-50" rows="10" id="content" name="content" readOnly="readOnly" value={content} ></textarea>
 				</div>
 			</form>
-			<div className="d-flex">
-				<div className="p-2 flex-fill d-grid">
-					<a href="./board_edit.html" className="btn btn-primary">수정</a>
+			{
+				mode &&
+				<div className="d-flex">
+					<div className="p-2 flex-fill d-grid">
+						<button type="button" onClick={() => nav(`/boardedit/${param}`)} className="btn btn-primary">수정</button>
+					</div>
+					<div className="p-2 flex-fill d-grid">
+						<button type="button" onClick={() => boardDelClick()} className="btn btn-primary">삭제</button>
+					</div>
+					<div className="p-2 flex-fill d-grid">
+						<button type="button" onClick={() => nav('/')} className="btn btn-primary">취소</button>
+					</div>
 				</div>
-				<div className="p-2 flex-fill d-grid">
-					<a href="../index.html" className="btn btn-primary">삭제</a>
+			}
+			{
+				!mode &&
+				<div className="d-flex">
+					<div className="p-2 flex-fill d-grid">
+						<button type="button" onClick={() => nav('/')} className="btn btn-primary">목록</button>
+					</div>
 				</div>
-				<div className="p-2 flex-fill d-grid">
-					<a href="../index.html" className="btn btn-primary">취소</a>
-				</div>
-			</div>
-			<div className="comment-box mb-4">
+			}
+			<div className="comment-box mb-4" onLoad={onload()}>
 				<div className="d-flex">
 					<div className="profile-img"></div>
 
 					<div className="flex-grow-1 mt-3 position-relative">
-						<textarea type="text" className="form-control auto-resize" style={styles} rows="1" id="comment_area" name="comment_area"></textarea>
-						<input type="text" className="form-control comment-input" maxLength="3000" placeholder="댓글을 입력하세요" />
+						<textarea type="text" className="form-control auto-resize" style={styles} rows="1" id="comment_area" name="comment_area" value={commentCont} onChange={e => setCommentCont(e.target.value)}></textarea>
 					</div>
-					<button className="btn btn-success btn-sm bottom-0 start- mx-2 mt-3">
+					<button className="btn btn-success btn-sm bottom-0 start- mx-2 mt-3" onClick={() => addComment()}>
 						등록
 					</button>
 				</div>
 			</div>
 			<div>
+
 				{/* <!-- 댓글  --> */}
-				<div className="comments my-3 w-100 pb-2">
-					<div className="d-flex align-items-start">
-						{/* <!-- 프로필 이미지 --> */}
-						<img src="../img01.jpg" className="rounded-circle me-3" width="50" height="50" alt="profile" />
-						{/* <!-- 댓글 내용 --> */}
-						<div className="flex-grow-1">
-							<div className="d-flex justify-content-between align-items-center">
-								<div className="fw-bold">최윤우짱짱맨</div>
-								<div>
-									<button className="btn btn-outline-secondary btn-sm me-1">수정</button>
-									<button className="btn btn-outline-danger btn-sm">삭제</button>
+				{
+					commentList.map((v, i) =>
+						<div className="comments my-3 w-100 pb-2" key={i}>
+							<div className="d-flex align-items-start">
+								{/* <!-- 프로필 이미지 --> */}
+								<img src="../img01.jpg" className="rounded-circle me-3" width="50" height="50" alt="profile" />
+								{/* <!-- 댓글 내용 --> */}
+								<div className="flex-grow-1">
+									<div className="d-flex justify-content-between align-items-center">
+										<div className="fw-bold">{v.name}</div>
+										{
+											v.userEmail == userEmail &&
+											<div>
+												{
+													clickCom !== i &&
+													<div>
+														<button className="btn btn-outline-secondary btn-sm me-1" onClick={() => {
+															setClickCom(i)
+															setEditCom(v.comment)
+														}}>수정</button>
+														<button className="btn btn-outline-danger btn-sm" type="button" onClick={() => commentDelClick(v.no)}>삭제</button>
+													</div>
+												}
+												{
+													clickCom === i &&
+													<div>
+														<button className="btn btn-outline-secondary btn-sm me-1" onClick={() => {
+															setClickCom('')
+															commentEdit(v.no)
+														}}>저장</button>
+														<button className="btn btn-outline-danger btn-sm" type="button" onClick={() => {
+															setReset(!reset)
+															setEditCom('')
+														}}>취소</button>
+													</div>
+												}
+
+											</div>
+										}
+									</div>
+									{
+										clickCom !== i &&
+										<div className="mt-1">
+											{v.comment}
+										</div>
+									}
+									{
+										clickCom === i &&
+										<div className="flex-grow-1 mt-3 position-relative">
+											<textarea type="text" className="form-control auto-resize" style={styles} rows="1" id="comment_area" name="comment_area" value={editCom} onChange={e => setEditCom(e.target.value)}></textarea>
+										</div>
+									}
+									<div className="text-muted small my-1">
+										{v.modDate.split('T')[0]} {v.modDate.split('T')[1]}
+									</div>
 								</div>
 							</div>
-							<div className="mt-1">
-								저는 귀여워요
-							</div>
-							<div className="text-muted small my-1">
-								2026.02.06 16:56
-							</div>
-						</div>
-					</div>
-				</div>
-				<div className="comments my-3 w-100 pb-2">
-					<div className="d-flex align-items-start">
-						{/* <!-- 프로필 이미지 --> */}
-						<img src="../img01.jpg" className="rounded-circle me-3" width="50" height="50" alt="profile" />
-						{/* <!-- 댓글 내용 --> */}
-						<div className="flex-grow-1">
-							<div className="d-flex justify-content-between align-items-center">
-								<div className="fw-bold">최윤우땐씽머씐</div>
-								<div>
-									<button className="btn btn-outline-secondary btn-sm me-1">수정</button>
-									<button className="btn btn-outline-danger btn-sm">삭제</button>
-								</div>
-							</div>
-							<div className="mt-1">
-								저는 왕짱깜찍이에용 &gt;&lt; 뿌우 </div>
-							<div className="text-muted small my-1">
-								2026.02.06 16:56
-							</div>
-						</div>
-					</div>
-				</div>
+						</div>)
+
+				}
 			</div>
 		</div>
 	)
